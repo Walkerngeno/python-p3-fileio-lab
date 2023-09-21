360416138e423fe41ced8cc6cbe89ab5dc463c8f
Walkerngeno <ngenobrayo7@gmail.com>
2023-09-21 20:48:30 +0300

    update changes

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,25 @@
+# In lib/file_io.py
+
 def write_file(file_name, file_content):
-    pass
+    # Append .txt extension to the file_name
+    file_name_with_extension = f"{file_name}.txt"
+    
+    # Open the file for writing and write the content
+    with open(file_name_with_extension, 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    # Append .txt extension to the file_name
+    file_name_with_extension = f"{file_name}.txt"
+    
+    # Open the file for appending and append the content
+    with open(file_name_with_extension, 'a') as file:
+        file.write("\n" + append_content)
 
 def read_file(file_name):
-    pass
+    # Append .txt extension to the file_name
+    file_name_with_extension = f"{file_name}.txt"
+    
+    # Open the file for reading and return its content
+    with open(file_name_with_extension, 'r') as file:
+        return file.read()
